9cb67c5d94036c7fa2ced8596615f2fcb9a72bf9
zach-ctrl <146124415+zach-ctrl@users.noreply.github.com>
2023-11-30 21:35:26 -0500

    Update Python Queue (1).py

--- Python Queue (1).py
+++ Python Queue (1).py
@@ -1,33 +1,17 @@
-# Python program to  
-# demonstrate queue implementation 
-# using list 
-  
-# Initializing a queue 
-queue = [] 
-  
-# Adding elements to the queue 
-queue.append('a') 
-queue.append('b') 
-queue.append('c') 
-  
-print ("Queues are First-In First-Out (FIFO) data structures") 
-print("\nInitial queue. Used queue.append(item) to enter elements into the queue") 
-print(queue) 
-  
-# Removing elements from the queue 
-print("\nElements dequeued from queue. Used queue.pop(0) to pop element in front") 
-print(queue.pop(0)) 
-print(queue.pop(0))
-print("\nElement 'd' enqueued (appended)")
-queue.append('d')
-print (queue)
-print("\nElements dequeued from queue") 
-print(queue.pop(0)) 
-print(queue.pop(0))
-  
-print("\nQueue after removing elements") 
-print(queue) 
-  
-# Uncommenting print(queue.pop(0)) 
-# will raise and IndexError 
-# as the queue is now empty 
+names = ['Joe', 'Sally', 'Jim', 'June', 'Betty', 'Bill', 'Sara', 'Zak', 'Anne', 'Kate']
+
+
+print(names.pop(0))
+print(names.pop(1))
+print(names.pop(2))
+print(names.pop(3))
+print(names.pop(4))
+
+
+names = ['Zak', 'Sally', 'June', 'Bill', 'Kate',]
+
+print(names.pop(0))
+print(names.pop(1))
+print(names.pop(2))
+print(names.pop(3))
+print(names.pop(4))
